74df9029781285ace2bd2db14da4f8251d9b177e
Deborah <datjin04@gmail.com>
2018-04-11 10:54:39 +0200

    Javascript adjustments

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var myMap;
+  var myMap;
   var marker1;
   var marker2;
   var marker3;
@@ -6,7 +6,9 @@ var myMap;
   var geoJSON;
   var request;
   var gettingData = false;
-  var openWeatherMapKey = "b93f0214e63748be2fedba711c6f1709"
+  var openWeatherMapKey = "b93f0214e63748be2fedba711c6f1709"//Mijn OWM key
+  
+  // Functie om de Google map aan te maken
   function initMap() {
 
     var myStyles = [
@@ -269,10 +271,13 @@ var myMap;
       zoom: 12,
       styles: myStyles
     };
+
     myMap = new google.maps.Map(document.getElementById('map'),
         mapOptions);
+
     // Add interaction listeners to make weather requests
     google.maps.event.addListener(myMap, 'idle', checkIfDataRequested);
+
     // Sets up and populates the info window with details
     myMap.data.addListener('click', function(event) {
       infowindow.setContent(
@@ -295,273 +300,281 @@ var myMap;
     });
 
     // marker for kennedy space marker
-  marker1 = new google.maps.Marker({
-    position: {
-      lat: 28.572872, 
-      lng: -80.6489808,
-    },
-    map: myMap,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    title: 'Kennedy Space Center',
-    icon: 'img/satellite.png'
-  });
-  // marker1.addListener('click', toggleBounce);
+    marker1 = new google.maps.Marker({
+      position: {
+        lat: 28.572872, 
+        lng: -80.6489808,
+      },
+      map: myMap,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      title: 'Kennedy Space Center',
+      icon: 'img/satellite.png'
+    });
+    // marker1.addListener('click', toggleBounce);
 
-  // marker for best landing space
-  marker2 = new google.maps.Marker({
-    position: {
-      lat: 28.614458, 
-      lng: -80.694108,
-    },
-    map: myMap,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    icon: 'img/landing.png'
-  });
-  marker2.addListener('click', toggleBounce);
+    // marker for best landing space
+    marker2 = new google.maps.Marker({
+      position: {
+        lat: 28.614458, 
+        lng: -80.694108,
+      },
+      map: myMap,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      icon: 'img/landing.png'
+    });
+    marker2.addListener('click', toggleBounce);
 
-  // marker for best landing space
-  marker3 = new google.maps.Marker({
-    position: {
-      lat: 27.652590, 
-      lng: -81.350927,
-    },
-    map: myMap,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    icon: 'img/landing.png'
-  });
-  marker3.addListener('click', toggleBounce);
+    // marker for best landing space
+    marker3 = new google.maps.Marker({
+      position: {
+        lat: 27.652590, 
+        lng: -81.350927,
+      },
+      map: myMap,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      icon: 'img/landing.png'
+    });
+    marker3.addListener('click', toggleBounce);
 
-  // marker for best landing space
-  marker4 = new google.maps.Marker({
-    position: {
-      lat: 28.488672, 
-      lng: -80.572824,
-    },
-    map: myMap,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    icon: 'img/landing.png'
-  });
-  marker4.addListener('click', toggleBounce);
+    // marker for best landing space
+    marker4 = new google.maps.Marker({
+      position: {
+        lat: 28.488672, 
+        lng: -80.572824,
+      },
+      map: myMap,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      icon: 'img/landing.png'
+    });
+    marker4.addListener('click', toggleBounce);
   }
 
-function toggleBounce() {
+  function toggleBounce() {
 
-    if (marker2.getAnimation() !== null) {
-        marker2.setAnimation(null);
-    } else {
-        marker2.setAnimation(google.maps.Animation.BOUNCE);
-    }
+      if (marker2.getAnimation() !== null) {
+          marker2.setAnimation(null);
+      } else {
+          marker2.setAnimation(google.maps.Animation.BOUNCE);
+      }
 
-    if (marker3.getAnimation() !== null) {
-        marker3.setAnimation(null);
-    } else {
-        marker3.setAnimation(google.maps.Animation.BOUNCE);
-    }
+      if (marker3.getAnimation() !== null) {
+          marker3.setAnimation(null);
+      } else {
+          marker3.setAnimation(google.maps.Animation.BOUNCE);
+      }
 
-    if (marker4.getAnimation() !== null) {
-        marker4.setAnimation(null);
-    } else {
-        marker4.setAnimation(google.maps.Animation.BOUNCE);
-    }
-}
+      if (marker4.getAnimation() !== null) {
+          marker4.setAnimation(null);
+      } else {
+          marker4.setAnimation(google.maps.Animation.BOUNCE);
+      }
+  }
 
-  var checkIfDataRequested = function() {
-    // Stop extra requests being sent
-    while (gettingData === true) {
-      request.abort();
-      gettingData = false;
-    }
-    getCoords();
-  };
-  // Get the coordinates from the Map bounds
-  var getCoords = function() {
-    var bounds = myMap.getBounds();
-    var NE = bounds.getNorthEast();
-    var SW = bounds.getSouthWest();
-    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
-  };
-  // Make the weather request
-  var getWeather = function(northLat, eastLng, southLat, westLng) {
-    gettingData = true;
-    var requestString = "https://api.openweathermap.org/data/2.5/box/city?bbox="
-                        + westLng + "," + northLat + "," //left top
-                        + eastLng + "," + southLat + "," //right bottom
-                        + myMap.getZoom()
-                        + "&cluster=yes&format=json"
-                        + "&APPID=" + openWeatherMapKey;
-    request = new XMLHttpRequest();
-    request.onload = proccessResults;
-    request.open("get", requestString, true);
-    request.send();
-  };
-  // Take the JSON results and proccess them
-  var proccessResults = function() {
-    console.log(this);
-    var results = JSON.parse(this.responseText);
-    if (results.list.length > 0) {
-        resetData();
-        for (var i = 0; i < results.list.length; i++) {
-          geoJSON.features.push(jsonToGeoJson(results.list[i]));
-        }
-        drawIcons(geoJSON);
-    }
-  };
-  var infowindow = new google.maps.InfoWindow();
-  // For each result that comes back, convert the data to geoJSON
-  var jsonToGeoJson = function (weatherItem) {
-    var feature = {
-      type: "Feature",
-      properties: {
-        city: weatherItem.name,
-        weather: weatherItem.weather[0].main,
-        temperature: weatherItem.main.temp,
-        min: weatherItem.main.temp_min,
-        max: weatherItem.main.temp_max,
-        humidity: weatherItem.main.humidity,
-        pressure: weatherItem.main.pressure,
-        windSpeed: weatherItem.wind.speed,
-        windDegrees: weatherItem.wind.deg,
-        windGust: weatherItem.wind.gust,
-        icon: "http://openweathermap.org/img/w/"
-              + weatherItem.weather[0].icon  + ".png",
-        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+    function checkIfDataRequested() {
+      // Stop extra requests being sent
+      while (gettingData === true) {
+        request.abort();
+        gettingData = false;
+      }
+      getCoords();
+    };
+
+    // Get the coordinates from the Map bounds
+    function getCoords() {
+      var bounds = myMap.getBounds();
+      var NE = bounds.getNorthEast();
+      var SW = bounds.getSouthWest();
+      getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
+    };
+
+    // Make the weather request
+    function getWeather(northLat, eastLng, southLat, westLng) {
+      gettingData = true;
+      var requestString = "https://api.openweathermap.org/data/2.5/box/city?bbox="
+                          + westLng + "," + northLat + "," //left top
+                          + eastLng + "," + southLat + "," //right bottom
+                          + myMap.getZoom()
+                          + "&cluster=yes&format=json"
+                          + "&APPID=" + openWeatherMapKey;
+      request = new XMLHttpRequest();
+      request.onload = proccessResults;
+      request.open("get", requestString, true);
+      request.send();
+    };
+
+    // Take the JSON results and proccess them
+    function proccessResults() {
+      // console.log(this);
+      var results = JSON.parse(this.responseText);
+      if (results.list.length > 0) {
+          resetData();
+          for (var i = 0; i < results.list.length; i++) {
+            geoJSON.features.push(jsonToGeoJson(results.list[i]));
+          }
+          drawIcons(geoJSON);
       }
     };
-    // Set the custom marker icon
-    myMap.data.setStyle(function(feature) {
-      return {
-        icon: {
-          url: feature.getProperty('icon'),
-          anchor: new google.maps.Point(25, 25)
+
+    var infowindow = new google.maps.InfoWindow();
+    
+    // For each result that comes back, convert the data to geoJSON
+    function jsonToGeoJson(weatherItem) {
+      var feature = {
+        type: "Feature",
+        properties: {
+          city: weatherItem.name,
+          weather: weatherItem.weather[0].main,
+          temperature: weatherItem.main.temp,
+          min: weatherItem.main.temp_min,
+          max: weatherItem.main.temp_max,
+          humidity: weatherItem.main.humidity,
+          pressure: weatherItem.main.pressure,
+          windSpeed: weatherItem.wind.speed,
+          windDegrees: weatherItem.wind.deg,
+          windGust: weatherItem.wind.gust,
+          icon: "http://openweathermap.org/img/w/"
+                + weatherItem.weather[0].icon  + ".png",
+          coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
         }
       };
-    });
-    // returns object
-    return feature;
-  };
-  // Add the markers to the map
-  var drawIcons = function (weather) {
-     myMap.data.addGeoJson(geoJSON);
-     // Set the flag to finished
-     gettingData = false;
-  };
-  // Clear data layer and geoJSON
-  var resetData = function () {
-    geoJSON = {
-      type: "FeatureCollection",
-      features: []
+
+      // Set the custom marker icon
+      myMap.data.setStyle(function(feature) {
+        return {
+          icon: {
+            url: feature.getProperty('icon'),
+            anchor: new google.maps.Point(25, 25)
+          }
+        };
+      });
+      // returns object
+      return feature;
     };
-    myMap.data.forEach(function(feature) {
-      myMap.data.remove(feature);
+
+    // Add the markers to the map
+    function drawIcons(weather) {
+       myMap.data.addGeoJson(geoJSON);
+       // Set the flag to finished
+       gettingData = false;
+    };
+
+    // Clear data layer and geoJSON
+    function resetData() {
+      geoJSON = {
+        type: "FeatureCollection",
+        features: []
+      };
+      myMap.data.forEach(function(feature) {
+        myMap.data.remove(feature);
+      });
+    };
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+  // --------Begin Openweather API------------
+  /**
+   * Fetch API data
+   */
+  function getAPIdata() {
+    
+    var url = "https://api.openweathermap.org/data/2.5/forecast";
+    var apiKey ="b93f0214e63748be2fedba711c6f1709";
+    var city = "florida";
+
+    //Test OWM weaterlayers
+    //var weatherLayer = "https://tile.openweathermap.org/map/{clouds_new}/10/5/5.png?appid={b93f0214e63748be2fedba711c6f1709}";
+
+    // construct request
+    var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
+    
+    // get weather forecast
+    fetch(request)
+
+    // parse to JSON format
+    .then(function(response) {
+      return response.json();
+    })
+    
+    // render weather per day
+    .then(function(response) {
+
+      // render weatherCondition
+      onAPISucces(response);
+    })
+    
+    // catch error
+    .catch(function (error) {
+      // onAPIError();
+      console.error('Request failed', error);
     });
-  };
-  google.maps.event.addDomListener(window, 'load', initMap);
-
-// --------Begin Openweather API------------
-/**
- * Fetch API data
- */
-function getAPIdata() {
-  
-  var url = "https://api.openweathermap.org/data/2.5/forecast";
-  var apiKey ="b93f0214e63748be2fedba711c6f1709";
-  var city = "florida";
-
-  //Test OWM weaterlayers
-  //var weatherLayer = "https://tile.openweathermap.org/map/{clouds_new}/10/5/5.png?appid={b93f0214e63748be2fedba711c6f1709}";
-
-  // construct request
-  var request = url + "?" + "appid=" + apiKey + "&" + "q=" + city;
-  
-  // get weather forecast
-  fetch(request)
-
-  // parse to JSON format
-  .then(function(response) {
-    return response.json();
-  })
-  
-  // render weather per day
-  .then(function(response) {
-
-    // render weatherCondition
-    onAPISucces(response);
-  })
-  
-  // catch error
-  .catch(function (error) {
-    // onAPIError();
-    console.error('Request failed', error);
-  });
-}
-
-/**
- * Render weather listing
- */
-function onAPISucces(response) {
-
-  var weatherList = response.list;
-  var weatherBox = document.getElementById('weather');
-
-  for(var i=0; i< weatherList.length; i++){
-    //console.log(weatherList[i].main.temp - 273.15);
-
-    var dateTime = new Date(weatherList[i].dt_txt);
-    var date = formDate(dateTime);
-    var time = formTime(dateTime);
-    var temp = Math.floor(weatherList[i].main.temp - 273.15);
-    var iconUrl = 'http://openweathermap.org/img/w/'+weatherList[i].weather[0].icon+'.png';
-
-    forecastMessage =  '<div class="forecastMoment">';
-    forecastMessage +=   '<div class="date"> '+date+' </div>';
-    forecastMessage +=   '<div class="time"> '+ time +' </div>';
-    forecastMessage +=   '<div class="temp"> '+temp+'&#176;C </div>';
-    forecastMessage +=   '<div class="icon"> <img src="'+iconUrl+'"> </div>';
-    forecastMessage += '</div>';
-
-    weatherBox.innerHTML += forecastMessage;
   }
-}
 
-/**
- * Error
- */
-function updateUIError() {
-  var weatherBox = document.getElementById('weather');
-  weatherBox.className = 'hidden'; 
-}
+  /**
+   * Render weather listing
+   */
+  function onAPISucces(response) {
 
-/**
- * Format date
- */
-function formDate(date) {
-  var day = date.getDate();
-  var month = date.getMonth() + 1;
-  return day +'/'+ month;
-}
+    var weatherList = response.list;
+    var weatherBox = document.getElementById('weather');
 
-/**
- * Format time
- */
-function formTime(date) {
-  var hours = date.getHours();
-  if(hours<10){
-    hours = '0'+hours;
+    for(var i=0; i< weatherList.length; i++){
+      //console.log(weatherList[i].main.temp - 273.15);
+
+      var dateTime = new Date(weatherList[i].dt_txt);
+      var date = formDate(dateTime);
+      var time = formTime(dateTime);
+      var temp = Math.floor(weatherList[i].main.temp - 273.15);
+      var iconUrl = 'http://openweathermap.org/img/w/'+weatherList[i].weather[0].icon+'.png';
+
+      forecastMessage =  '<div class="forecastMoment">';
+      forecastMessage +=   '<div class="date"> '+date+' </div>';
+      forecastMessage +=   '<div class="time"> '+ time +' </div>';
+      forecastMessage +=   '<div class="temp"> '+temp+'&#176;C </div>';
+      forecastMessage +=   '<div class="icon"> <img src="'+iconUrl+'"> </div>';
+      forecastMessage += '</div>';
+
+      weatherBox.innerHTML += forecastMessage;
+    }
   }
-  var minutes = date.getMinutes();
-  if(minutes < 10){
-    minutes = '0'+ minutes;
-  }
-  return hours +':'+ minutes;
-}
 
-// init data stream
-getAPIdata();
+  /**
+   * Error
+   */
+  function updateUIError() {
+    var weatherBox = document.getElementById('weather');
+    weatherBox.className = 'hidden'; 
+  }
+
+  /**
+   * Format date
+   */
+  function formDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    return day +'/'+ month;
+  }
+
+  /**
+   * Format time
+   */
+  function formTime(date) {
+    var hours = date.getHours();
+    if(hours<10){
+      hours = '0'+hours;
+    }
+    var minutes = date.getMinutes();
+    if(minutes < 10){
+      minutes = '0'+ minutes;
+    }
+    return hours +':'+ minutes;
+  }
+
+  // init data stream
+  getAPIdata();
